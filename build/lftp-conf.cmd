f11a6833c994a74a6a61537f926cd3e0a33cd458
Francesco Vigo <fvigo@paloaltonetworks.com>
2018-04-08 15:37:58 -0700

    new travis

--- build/lftp-conf.cmd
+++ build/lftp-conf.cmd
@@ -4,4 +4,5 @@ set ftp:ssl-protect-data true;
 open ftps://ftp.box.com:990
 user BOXUSER BOXPASSWORD
 cd Business\ Development\ -\ Shared/Francesco/RefAppCFTBuckets;
-put buckets/appframework-conf.zip
+put buckets/appframework-conf.zip;
+exit;
--- build/lftp-ngfw.cmd
+++ build/lftp-ngfw.cmd
@@ -4,4 +4,5 @@ set ftp:ssl-protect-data true;
 open ftps://ftp.box.com:990
 user BOXUSER BOXPASSWORD
 cd Business\ Development\ -\ Shared/Francesco/RefAppCFTBuckets;
-put buckets/appframework-ngfw.zip
+put buckets/appframework-ngfw.zip;
+exit;
