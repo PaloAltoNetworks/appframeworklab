fc01151692be530a1d05119de3dc1747f7ec2ab9
Francesco Vigo <fvigo@paloaltonetworks.com>
2018-04-08 17:55:11 -0700

    ok

--- build/lftp-cft.cmd
+++ build/lftp-cft.cmd
@@ -4,4 +4,5 @@ set ftp:ssl-protect-data true;
 open ftps://ftp.box.com:990
 user BOXUSER BOXPASSWORD
 cd Business\ Development\ -\ Shared/Francesco/RefAppCFTBuckets;
-put cft/*.json;
+put cft/appframework-lab.json;
+put cft/apiexplorer-cft.json;
