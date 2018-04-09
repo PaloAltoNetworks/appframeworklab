2753d413d0058604c2c777e85d7018c85c40f47f
Francesco Vigo <fvigo@paloaltonetworks.com>
2018-04-08 19:18:53 -0700

    moved to curl for upload

--- build/lftp-cft.cmd
+++
@@ -1,8 +0,0 @@
-set ftps:initial-prot "";
-set ftp:ssl-force true;
-set ftp:ssl-protect-data true;
-open ftps://ftp.box.com:990
-user BOXUSER BOXPASSWORD
-cd "GitUpload";
-put "cft/appframework-lab.json";
-put "cft/apiexplorer-cft.json";
--- build/lftp-conf.cmd
+++
@@ -1,7 +0,0 @@
-set ftps:initial-prot "";
-set ftp:ssl-force true;
-set ftp:ssl-protect-data true;
-open ftps://ftp.box.com:990
-user BOXUSER BOXPASSWORD
-cd "GitUpload";
-put "buckets/appframework-conf.zip";
