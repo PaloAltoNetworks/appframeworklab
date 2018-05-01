fbe46415ad300024f22983ebdce4d82d1e79e072
Francesco Vigo <fvigo@paloaltonetworks.com>
2018-05-01 00:27:44 -0700

    Dockerized CFT and bugfixes

--- build/build.sh
+++ build/build.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-BRES=0
-echo "Checking if resources have changed..."
-git diff-tree --quiet HEAD HEAD^ -r resources || BRES=1
-if [[ "$BRES" == "1" ]]; then
-  echo "Resources have changed, regenerating TAR.GZ file..."
-  tar cvfz buckets/appframework-conf/opt/apiexplorer.tar.gz resources
-  BCONF=1
-else
-  BCONF=0
-fi
-
 echo "Checking if CONF bucket has changed..."
 if [[ "$BCONF" == "0" ]]; then
   git diff-tree --quiet HEAD HEAD^ -r buckets/appframework-conf || BCONF=1
