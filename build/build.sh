0181f843326f86d5ac5a29b17c8f10b1e6444100
Francesco Vigo <fvigo@paloaltonetworks.com>
2018-10-29 13:23:20 -0700

    fixed travis autobuild

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Checking if CONF bucket has changed..."
 if [[ "$BCONF" == "0" ]]; then
+  echo "Running git diff-tree"
+  git diff-tree HEAD HEAD^ -r buckets/appframework-conf
   git diff-tree --quiet HEAD HEAD^ -r buckets/appframework-conf || BCONF=1
 fi
 if [[ "$BCONF" == "1" ]]; then
