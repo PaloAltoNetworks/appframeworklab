e3cd6372c8f5776b788d082c20319da1fe4fee1d
Francesco Vigo <fvigo@paloaltonetworks.com>
2018-10-29 13:44:41 -0700

    fixed webgenconfig.py indentation

--- buckets/appframework-conf/opt/webgenconfig.py
+++ buckets/appframework-conf/opt/webgenconfig.py
@@ -19,7 +19,6 @@ rootURLs = [
 		"https://www.jpmorgan.com",
 		"https://www.paloaltonetworks.com",
 		"https://www.avherald.com",
-                "https://www.apple.com",
 		"https://en.wikipedia.org/wiki/Main_Page",
 		"https://austin.craigslist.org/"
         ]
